8cdbe2b80063f81591328a9024398ec3c000f7c7
futuralight <futuralight@outlook.com>
2021-03-01 20:19:55 +0500

    env gitignore

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/futuralight/weather-station/sensor"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -26,4 +29,21 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(fmt.Sprintf("Влажность %f", h))
+}
+
+func init() {
+	err := loadEnv()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func loadEnv() error {
+	godotenv.Load()
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return err
+	}
+	err = godotenv.Load(dir + "/.env") //Загрузка .env файла
+	return nil
 }
