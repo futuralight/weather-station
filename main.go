1dfc46a9e8bb99b48a9f554cc99758e26f27174d
futuralight <futuralight@outlook.com>
2021-03-01 13:10:47 +0500

    no testing

--- main.go
+++ main.go
@@ -3,53 +3,27 @@ package main
 import (
 	"fmt"
 
-	"github.com/d2r2/go-bsbmp"
-	"github.com/d2r2/go-i2c"
-	"github.com/d2r2/go-logger"
+	"github.com/futuralight/weather-station/sensor"
 )
 
 func main() {
-	defer logger.FinalizeLogger()
-	// Create new connection to i2c-bus on 1 line with address 0x76.
-	// Use i2cdetect utility to find device address over the i2c-bus
-	i2c, err := i2c.NewI2C(0x76, 1)
+	sensor, err := sensor.CreateBme280Sensor(1)
 	if err != nil {
 		panic(err)
 	}
-	defer i2c.Close()
-	// Uncomment/comment next lines to suppress/increase verbosity of output
-	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
-	logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
-	sensor, err := bsbmp.NewBMP(bsbmp.BME280, i2c) // signature=0x60
-	if err != nil {
-		panic(err)
-
-	}
-	id, err := sensor.ReadSensorID()
-	if err != nil {
-		panic(err)
-
-	}
-	fmt.Println(id)
-	err = sensor.IsValidCoefficients()
-	if err != nil {
-		panic(err)
-
-	}
-	// Read temperature in celsius degree
-	t, err := sensor.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
-	fmt.Println(fmt.Sprintf("Температура %fС", t))
-
-	supported, h1, err := sensor.ReadHumidityRH(bsbmp.ACCURACY_STANDARD)
-	if supported {
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(fmt.Sprintf("Влажность = %v %%", h1))
-	}
-	p, err := sensor.ReadPressureMmHg(bsbmp.ACCURACY_STANDARD)
+	t, err := sensor.SenseTemperature()
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(fmt.Sprintf("Давление = %f %%", p))
+	fmt.Println(fmt.Sprintf("Температура %f", t))
+	p, err := sensor.SensePressure()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fmt.Sprintf("Давление %f", p))
+	h, err := sensor.SenseHumidity()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(fmt.Sprintf("Влажность %f", h))
 }
