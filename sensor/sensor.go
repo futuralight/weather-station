8f876a3c0ce6bc57b4e087c814c7fa1480a495d1
futuralight <futuralight@outlook.com>
2021-06-04 12:56:18 +0500

    ref

--- sensor/sensor.go
+++ sensor/sensor.go
@@ -13,7 +13,7 @@ type Sensitive interface {
 	SenseHumidity() (float32, error)
 }
 
-//BME280Sensor - 280 wrapper
+//BME280Sensor - 280 wrapper struct
 type BME280Sensor struct {
 	sensor *bsbmp.BMP
 }
@@ -28,28 +28,26 @@ func (bme280 *BME280Sensor) SensePressure() (float32, error) {
 	return bme280.sensor.ReadPressureMmHg(bsbmp.ACCURACY_STANDARD)
 }
 
-//SenseHumidity get 280 Humidity
+//SenseHumidity get 280 humidity
 func (bme280 *BME280Sensor) SenseHumidity() (float32, error) {
 	_, hmd, err := bme280.sensor.ReadHumidityRH(bsbmp.ACCURACY_STANDARD)
 	return hmd, err
 }
 
 //CreateBme280Sensor - return brand new BME280Sensor
-func CreateBme280Sensor(bus int) (BME280Sensor, error) {
-
+func CreateBme280Sensor(bus int) (*BME280Sensor, error) {
 	defer logger.FinalizeLogger()
-	// Uncomment/comment next lines to suppress/increase verbosity of output
-	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
+	logger.ChangePackageLogLevel("i2c", logger.InfoLevel) //TODO убрать
 	logger.ChangePackageLogLevel("bsbmp", logger.InfoLevel)
-	var bme280Sensor BME280Sensor
 	i2c, err := i2c.NewI2C(0x76, bus)
 	if err != nil {
-		return bme280Sensor, err
+		return nil, err
 	}
-	sensor, err := bsbmp.NewBMP(bsbmp.BME280, i2c) // signature=0x60
+	sensor, err := bsbmp.NewBMP(bsbmp.BME280, i2c)
 	if err != nil {
-		return bme280Sensor, err
+		return nil, err
 	}
-	bme280Sensor.sensor = sensor
-	return bme280Sensor, nil
+	return &BME280Sensor{
+		sensor,
+	}, nil
 }
